176ac6bcdb8294bd8d51fafb768148d0fc69f435
bkvdm <vadim@991199.ru>
2023-12-21 17:26:08 +0300

    Домашнее задание: Алгоритмы (часть №1). Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75306/homework-requirements

---
+++ src/main/java/tel/bvm/algoritmLectionOne/AlgoritmLectionOneApplication.java
@@ -0,0 +1,13 @@
+package tel.bvm.algoritmLectionOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmLectionOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmLectionOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tel/bvm/algoritmLectionOne/AlgoritmLectionOneApplicationTests.java
@@ -0,0 +1,13 @@
+package tel.bvm.algoritmLectionOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmLectionOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
