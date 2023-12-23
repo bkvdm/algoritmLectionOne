b85c2956f37586cb0c8e6df994de95caa9a63aef
bkvdm <vadim@991199.ru>
2023-12-23 14:42:46 +0300

    Домашнее задание: Алгоритмы (часть №1). Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75306/homework-requirements

--- src/test/java/tel/bvm/algoritmLectionOne/StringListImplTest.java
+++ src/test/java/tel/bvm/algoritmLectionOne/StringListImplTest.java
@@ -1,13 +1,45 @@
 package tel.bvm.algoritmLectionOne;
 
+import net.bytebuddy.asm.MemberSubstitution;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringListImplTest {
 
-    @Test
-    void add() {
+    public static Stream<Arguments> add() {
+        return Stream.of(Arguments.of(11, 11),
+                Arguments.of(21, 21),
+                Arguments.of(50, 50),
+                Arguments.of(Integer.MAX_VALUE, ));
+    }
+
+@BeforeEach
+public void setUp() {
+    String[] storage = new String[]{
+            "0 - zero",
+            "1 - one",
+            "2 - two",
+            "3 - three",
+            "4 - four",
+            "5 - five",
+            "6 - six",
+            "7 - seven",
+            "8 - eight",
+            "9 - nine"};
+}
+
+    @ParameterizedTest
+    @MethodSource("add")
+    void addedVerify(String expected) {
+        assertEquals(expected, StringList.add(String ));
+
     }
 
     @Test
